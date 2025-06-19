d994dc677c1723bbe7ed8ad6823b3705123764d4
VORTEX <debankur.das.personal@gmail.com>
2025-06-20 02:35:58 +0530

    Basic portfolio landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 function App() {
   return (
-    <div className="flex items-center justify-center h-screen">
-      Hello World
+    <div className="flex flex-col items-center justify-center gap-4 h-screen">
+      <p className="text-base">Hello World</p>
+      <h1 className="text-2xl lg:text-5xl font-medium">I'm Debankur Das</h1>
+      <h2 className="text-lg lg:text-xl font-normal">a.k.a <span className="font-medium">VORTEX</span></h2>
     </div>
   )
 }
